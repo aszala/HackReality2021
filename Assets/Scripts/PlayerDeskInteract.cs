3fb0880c172868cefb543de48babda7b2a244c73
Abhay Zala <zala.abhay@gmail.com>
2021-03-15 03:29:56 -0400

    Add files via upload

---
+++ Assets/Scripts/PlayerDeskInteract.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeskInteract : MonoBehaviour {
+
+    private List<string> objects = new List<string>();
+
+
+	void Update() {
+        bool hand = false, virus = false;
+
+        foreach (string s in objects) {
+            if (s.Contains("Hand")) {
+                hand = true;
+            }
+
+            if (s.Contains("Virus")) {
+                virus = true;
+			}
+		}
+
+        if (hand && virus) {
+            // Load new scene
+		}
+	}
+
+	public void OnTriggerEnter(Collider other) {
+        objects.Add(other.name);
+    }
+
+    public void OnTriggerExit(Collider other) {
+        objects.Remove(other.name);
+    }
+}
